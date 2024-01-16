aa8ee5e8d3745d50e938cfcfd8a5fd02e572e41c
yerasirakeshreddy <109621785+yerasirakeshreddy@users.noreply.github.com>
2024-01-16 12:58:48 +0530

    Add files via upload

---
+++ trail_1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().system('pip install streamlit')
+
+
+# In[2]:
+
+
+import streamlit as st
+
+
+# In[3]:
+
+
+st.title("hello streamlit")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
